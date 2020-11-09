4f8047a98cb848325147abe21b79918fb1ee0b17
bachlx01 <73922823+bachlx01@users.noreply.github.com>
2020-11-09 14:05:13 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var inputWidth = prompt('Enter the Width');
+var inputHeight = prompt('Enter the Height');
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+var area = width * height;
+document.write('Input width is: ' + width + '<br/>');
+document.write('Input height is: ' + height + '<br/>');
+document.write('The area is: ' + area);
